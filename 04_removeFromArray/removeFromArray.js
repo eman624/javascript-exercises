58801d24819be89a7f1b66e9aded39b7cdd04e4e
Edgar <edgar.noel.m@gmail.com>
2024-03-17 16:34:35 -0700

    passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  const arr = Array.from(arguments[0]);
+  const numWordFilter = Array.prototype.slice.call(arguments, 1);
 
+  const result = arr.filter((item) => !numWordFilter.includes(item));
+  console.log("result: " + result);
+  return result;
 };
 
 // Do not edit below this line
