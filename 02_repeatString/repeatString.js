2b60525a02e91e2a8c42ab0edef9ea293d24b2d8
Edgar <edgar.noel.m@gmail.com>
2024-03-17 00:43:09 -0700

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  let str = "";
 
+  if (times < 0) return "ERROR";
+
+  for (let i = 1; i <= times; i++) {
+    str += word;
+  }
+  return str;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
