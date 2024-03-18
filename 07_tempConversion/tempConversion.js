e56b10589f4723c6c37bef4a01aa15da8dc4ad8a
Edgar <edgar.noel.m@gmail.com>
2024-03-17 17:55:16 -0700

    passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,14 +1,10 @@
 const convertToCelsius = function (fahrenheit) {
-  const celsius = (fahrenheit - 32) * (5 / 9);
-  return parseFloat(celsius.toFixed(1));
+  return parseFloat(((fahrenheit - 32) * (5 / 9)).toFixed(1));
 };
 
 const convertToFahrenheit = function (celsius) {
-  const fahrenheit = celsius * (9 / 5) + 32;
-  return parseFloat(fahrenheit.toFixed(1));
+  return parseFloat((celsius * (9 / 5) + 32).toFixed(1));
 };
-
-convertToCelsius(32);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
