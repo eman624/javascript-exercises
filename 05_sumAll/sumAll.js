c53564239efc3986481d4f5254683f0fb1ff8fdc
Edgar <edgar.noel.m@gmail.com>
2024-03-17 17:07:59 -0700

    passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0) {
+    return "ERROR";
+  }
+  if (isNaN(x) || isNaN(y) || typeof x === "string" || typeof y === "string") {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  if (x < y) {
+    for (var i = x; i <= y; i++) {
+      sum += i;
+    }
+  } else if (x > y) {
+    for (var i = y; i <= x; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
