bbe58d196849c7ff7e943bb451ac3727bbbf0a75
Edgar <edgar.noel.m@gmail.com>
2024-04-14 01:45:10 -0700

    passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,19 +7,25 @@ const subtract = function (x, y) {
 };
 
 const sum = function (arr) {
-  return arr.reduce((acc, curr) => acc + curr);
+  return arr.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
 };
 
-const multiply = function (x, y) {
-  return x * y;
+const multiply = function (arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
 const power = function (x, y) {
-  return power(x, y);
+  return Math.pow(x, y);
 };
 
-const factorial = function (x, y) {
-  return factorial(x, y);
+const factorial = function (x) {
+  if (x === 0 || x === 1) return 1;
+  for (var i = x - 1; i >= 1; i--) {
+    x *= i;
+  }
+  return x;
 };
 
 // Do not edit below this line
