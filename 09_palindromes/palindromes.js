936b6524558e13408633b4622e6286b1822ff851
Edgar <edgar.noel.m@gmail.com>
2024-04-14 02:50:49 -0700

    fomatted palindromes()

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,8 @@
 const palindromes = function (str) {
   const regex = /[^A-Za-z0-9]/g;
-
   const lowRegStr = str.toLowerCase().replace(regex, "");
   const reverseStr = lowRegStr.split("").reverse().join("");
 
-  console.log(reverseStr);
   return reverseStr === lowRegStr;
 };
 
