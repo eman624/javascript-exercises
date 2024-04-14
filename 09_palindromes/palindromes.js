1ce85affb763e6996cea2f71154b71450900e933
Edgar <edgar.noel.m@gmail.com>
2024-04-14 02:31:15 -0700

    code palindromes()

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const reverseStr = str.split("").reverse().join("");
+  return reverseStr === str;
 };
 
 // Do not edit below this line
